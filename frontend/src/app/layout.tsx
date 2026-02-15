af744f7d356acbaf5819fc0a6f37d8bede98946f
automania <lucasricardolebre1984@gmail.com>
2026-02-14 22:36:08 -0300

    Revert "Add Vercel Web Analytics to Next.js"

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import { Toaster } from 'sonner'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +22,6 @@ export default function RootLayout({
         {children}
         <Toaster position="top-right" richColors />
         <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   )
